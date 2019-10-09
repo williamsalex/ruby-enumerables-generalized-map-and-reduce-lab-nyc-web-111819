acdd7e44920e54ee247eaf244e25cd688792411e
Alexander Williams <williams_alex@protonmail.com>
2019-10-09 06:47:47 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,8 @@
-# Your Code Here
+def map(array, block):
+  new_array = []
+  count = 0
+  while count < array.length
+    new_array[count] = block(array[count])
+    count = count+1 
+  new_array
+    
