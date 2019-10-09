edf64a22ba500181488f40f7fba91e05cdeabb81
Alexander Williams <williams_alex@protonmail.com>
2019-10-09 06:47:25 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -2,5 +2,7 @@ def map(array, block):
   new_array = []
   count = 0
   while count < array.length
-    new_array[count] = array[count].block
+    new_array[count] = block(array[count])
+    count = count+1 
+  return new_array
     
